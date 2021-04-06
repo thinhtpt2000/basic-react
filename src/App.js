f500238616ec664f7cf158394f53ed6871a1060f
thinhtpt2000 <thinhtpt.2000@gmail.com>
2021-04-06 23:26:48 +0700

    [ThinhTPT] render list JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import logo from "./logo.svg";
 import './App.css';
+
+function showInfoProduct(product) {
+  if (product.status) {
+    return (
+      <div className={"product-" + product.id}>
+        <h2>Product Info</h2>
+        <h3>ID: {product.id}</h3>
+        <h3>Name: {product.name}</h3>
+        <h3>Price: {product.price} VND</h3>
+        <h3>Status: { product.status ? "Active" : "Pending"}</h3>
+      </div>
+    );
+  }
+}
 
 function App() {
   let a = 5;
@@ -9,58 +22,56 @@ function App() {
   let product = {
     id: 1,
     name: "iPhone 11 Pro Max",
-    price: 20000000
+    price: 20000000,
+    status: true
   };
+
+  let users = [
+    {
+      id: 1,
+      name: "ThinhTPT",
+      age: 21
+    },
+    {
+      id: 2,
+      name: "UyenDP",
+      age: 20
+    },
+    {
+      id: 3,
+      name: "ThaiTTQ",
+      age: 19
+    }
+  ]
+
+  let elements = users.map((user, index) => {
+    return (
+      <div key={index}>
+        <h2>{ user.name }</h2>
+        <h3>Age: { user.age }</h3>
+      </div>
+    );
+  });
 
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <a class="navbar-brand">JSX Components</a>
-        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <a className="navbar-brand">JSX Components</a>
+        <button className="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div id="my-nav" class="collapse navbar-collapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+        <div id="my-nav" className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Components</a>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Components</a>
             </li>
           </ul>
         </div>
       </nav>
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div className="card-group">
-              <div className="card">
-                <img className="card-img-top" src={logo} alt="Card cap" />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-              <div className="card">
-                <img className="card-img-top" src={logo} alt="Card cap" />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-              <div className="card">
-                <img className="card-img-top" src={logo} alt="Card cap" />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="container mt-2">
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <h2>Expression</h2>
@@ -76,10 +87,12 @@ function App() {
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <h2>Product Info</h2>
-            <h3>ID: {product.id}</h3>
-            <h3>Name: {product.name}</h3>
-            <h3>Price: {product.price} VND</h3>
+            {showInfoProduct(product)}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            {elements}
           </div>
         </div>
       </div>
