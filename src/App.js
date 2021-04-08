ceff23120a25a639009571b6534ff32ff40a2b3f
thinhtpt2000 <thinhtpt.2000@gmail.com>
2021-04-08 22:46:37 +0700

    [ThinhTPT] pratice state in functional components

--- src/App.js
+++ src/App.js
@@ -1,81 +1,80 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { useRef } from 'react';
+import { useState } from 'react';
 import './App.css';
-import Product from "./components/Product";
 
 function App() {
-  const pName = useRef(null);
-
-  let products = [
+  const [state, setState] = useState(
     {
-      id: 1,
-      name: "iPhone 11 Pro Max 64GB",
-      price: "2099000000",
-      image: "https://didongviet.vn/pub/media/catalog/product//9/9/99-min.jpg",
-      status: true
-    },
-    {
-      id: 2,
-      name: "iPhone 11 256GB",
-      price: "1859000000",
-      image: "https://didongviet.vn/pub/media/catalog/product/cache/926507dc7f93631a094422215b778fe0/i/p/iphone-11-tim_2.jpg",
-      status: false
-    },
-    {
-      id: 3,
-      name: "iPhone 12 Mini 64GB",
-      price: "2159000000",
-      image: "https://didongviet.vn/pub/media/catalog/product//i/p/iphone-12-mini-128gb_3.jpg",
-      status: true
+      products: [
+        {
+          id: 1,
+          name: "iPhone 11 Pro Max 64GB",
+          price: "2099000000",
+          image: "https://didongviet.vn/pub/media/catalog/product//9/9/99-min.jpg",
+          status: true
+        },
+        {
+          id: 2,
+          name: "iPhone 11 256GB",
+          price: "1859000000",
+          image: "https://didongviet.vn/pub/media/catalog/product/cache/926507dc7f93631a094422215b778fe0/i/p/iphone-11-tim_2.jpg",
+          status: false
+        },
+        {
+          id: 3,
+          name: "iPhone 12 Mini 64GB",
+          price: "2159000000",
+          image: "https://didongviet.vn/pub/media/catalog/product//i/p/iphone-12-mini-128gb_3.jpg",
+          status: true
+        }
+      ],
+      isActive: true
     }
-  ]
+  );
 
-  let productElements = products.map((p, index) => {
+  let productElements = state.products.map((p, index) => {
     return (
-      p.status &&
-      <Product
-        key={index}
-        price={p.price}
-        image={p.image}
-      >
-        {p.name}
-      </Product>
+      state.isActive &&
+      <tr key={index}>
+        <th scope="row">{index + 1}</th>
+        <td>{p.name}</td>
+        <td><span className="badge badge-success">{p.price} VND</span></td>
+      </tr>
     )
   });
 
-  const onClickBtn = () => {
-    console.log('This is app component');
-  }
-
-  const onAddProduct = () => {
-    console.log(pName.current.value);
+  const onSetActive = () => {
+    setState({
+      ...state,
+      isActive: !state.isActive
+    })
   }
 
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <a className="navbar-brand">Props</a>
+        <a className="navbar-brand">State</a>
         <button className="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
       </nav>
       <div className="container">
         <div className="row mt-5">
-          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div className="form-group">
-              <label htmlFor="name">Product name</label>
-              <input type="text" className="form-control" id="name" ref={pName}/>
-            </div>
-            <button type="submit" className="btn btn-primary" onClick={onAddProduct}>Save</button>
-          </div>
-        </div>
-        <div className="row mt-5">
-          {productElements}
-        </div>
-        <div className="row mt-5">
-          <button className="btn btn-warning" onClick={onClickBtn}>
-            Click Me !
-            </button>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productElements}
+            </tbody>
+          </table>
+          <button className="btn btn-warning" onClick={onSetActive}>
+            Active: {state.isActive ? "true" : "false"}
+          </button>
         </div>
       </div>
     </div>
