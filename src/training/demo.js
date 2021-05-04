c3e0eb0f986e20729d52d31f8def99fcdb990473
THINHTPT-PC\ThinhTPT <tthinh2128@gmail.com>
2021-05-04 22:27:49 +0700

    [ThinhTPT] practice with basic redux

--- src/training/demo.js
+++ src/training/demo.js
@@ -1,1 +1,39 @@
-console.log('Hello Redux');
+import { createStore } from 'redux';
+
+let initialState = {
+    status: false,
+    sort: {
+        by: 'name',
+        value: 1
+    }
+};
+
+let myReducer = (state = initialState, action) => {
+    if (action.type === 'TOGGLE_STATUS') {
+        state.status = !state.status;
+    } else if (action.type === 'SORT') {
+        state.sort = action.payload;
+    }
+    return state;
+};
+
+const store = createStore(myReducer);
+console.log('DEFAULT: ', store.getState());
+
+// Do toggle status
+let actionStatus = {
+    type: 'TOGGLE_STATUS'
+}
+store.dispatch(actionStatus);
+console.log('TOGGLE_STATUS: ', store.getState());
+
+// Do sort name Z->A
+let actionSort = {
+    type: 'SORT',
+    payload: {
+        by: 'name',
+        value: -1
+    }
+};
+store.dispatch(actionSort);
+console.log('SORT: ', store.getState());
